f4ee947c6379d920451f6abab0f16f671d8edaba
james-git <jamesc@ms5.url.com.tw>
2016-08-05 21:15:48 +0800

    Updated stringtranslate.models.py
    Change-Id: I6fc32f0436492ff69df78ba39175518c3c539225

--- stringtranslate/models.py
+++ stringtranslate/models.py
@@ -3,38 +3,38 @@ from django.utils import timezone
 
 
 class AppSet(models.Model):
-	name = models.CharField(max_length=50, unique=True)
+	name = models.CharField(max_length=50, unique=True, blank=False, null=False)
 
 	def __str__(self):
 		return self.name
 		
 	
 class LangCode(models.Model):
-	code = models.CharField(max_length=8, unique=True)
+	code = models.CharField(max_length=8, unique=True, blank=False, null=False)
 
 	def __str__(self):
 		return self.code
 		
 		
 class StringKey(models.Model):
-	app_set = models.ForeignKey('AppSet', blank=False, null=False)
-	string_key = models.CharField(max_length=50, unique=True)
+	string_key = models.CharField(max_length=50, unique=True, blank=False, null=False)
 	
 	def __str__(self):
 		return self.string_key
 	
 
 class StringTable(models.Model):
+	app_set = models.ForeignKey('AppSet')
+	lang_code = models.ForeignKey('LangCode')	
 	string_key = models.ForeignKey('StringKey')
-	lang_code = models.ForeignKey('LangCode', blank=False, null=False)
 	string_content = models.TextField()
 	created_date = models.DateTimeField(default=timezone.now)
 	modified_date = models.DateTimeField(blank=True, null=True)
 
 	class Meta:
-		unique_together = ('string_key', 'lang_code')
+		unique_together = ('app_set', 'string_key', 'lang_code')
 	
-	def save(self):
+	def commit(self):
 		self.modified_date = timezone.now()
 		self.save()
 		
