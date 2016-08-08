af377eacdd0207104ca483a4509b6079a4ccc594
james-git <jamesc@ms5.url.com.tw>
2016-08-08 15:29:46 +0800

    Updated both st_venv/urls.py and stringtranslate/urls.py to test url patterns.
    Change-Id: I830669d4f9027b6a6130c9ad01d68302347a877e

--- stringtranslate/urls.py
+++ stringtranslate/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+	url(r'post_list/', views.post_list, name='post_list'),
+	# url(r'^view_string_table/$', views.view_string_table, name='view_string_table'),
 ]
--- stringtranslate/views.py
+++ stringtranslate/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.shortcuts import render_to_response
+
+from .models import AppSet, LangCode, StringKey, StringTable
+
 
 def post_list(request):
-    return render(request, 'stringtranslate/post_list.html', {})
+    # return render(request, 'stringtranslate/post_list.html', {})
+	return render_to_response('stringtranslate/post_list.html', {})
+	
+def view_string_table(request):
+	return render_to_response('stringtranslate/view_string_table.html', {})
